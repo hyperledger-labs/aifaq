5883491c582f988974f2101501677d2f11abc58a
Supratick Mondal <91054530+sonustar@users.noreply.github.com>
2024-08-06 21:13:43 +0530

    Added an Api Emulator  (#38)
    * added
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    * added an Api emulator
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    * added modular routing
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    * Removed the binary files
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    * Seperated emulator router and rag router
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    * removed a uneccessary print statement
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    * Added the rag_router
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: sonustar <91054530+sonustar@users.noreply.github.com>

--- src/core/api.py
+++ src/core/api.py
@@ -1,18 +1,13 @@
 from utils import load_yaml_file
-from main import get_conversation
+from main_router import main_router
+from rag_router import rag_router
+from api_router import api_router 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 import uvicorn
 
 config_data = load_yaml_file("config.yaml")
-
-conversational_rag_chain = get_conversation()
-
-# define the Query class that contains the question
-class Query(BaseModel):
-    text: str
-
+    
 app = FastAPI()
 
 # Enable CORS
@@ -24,20 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# reply to GET requests, if the service is running
-@app.get("/")
-def hello():
-    return {"msg": "hello"}
 
-# reply to POST requests: '{"text": "How to install Hyperledger fabric?"}' 
-@app.post("/query")
-def answer(q: Query):
-    question = q.text
-    ai_msg_1 = conversational_rag_chain.invoke(
-        {"input": question}, 
-        config={"configurable": {"session_id": "1"}}, 
-        )["answer"]
-
-    return {"msg": ai_msg_1}
+# Include routers
+app.include_router(main_router)
+api_router.include_router(rag_router)
+app.include_router(api_router,prefix="/api")
 
 uvicorn.run(app,host=config_data["host"],port=config_data["port"])
