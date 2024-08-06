d15014320c83f05b4f23b563fcc59ac4322018af
Xitong(Jacqueline) Zhang <60076577+xtzhang1122@users.noreply.github.com>
2024-08-06 11:42:43 -0400

    Issue #20: Connect API to the frontend of the application (#29)
    * init integration
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>
    
    * update
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>
    
    * Revise based on feedback
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>
    
    * UI improvement
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>
    
    * UI fix
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>
    
    * moved bottom bar to the main page
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>
    
    ---------
    
    Signed-off-by: xtzhang1122 <xtzhang1122@hotmail.com>

--- src/frontend/components/chat-section.tsx
+++ src/frontend/components/chat-section.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import ChatRequest from './chat-request'
-import ChatResponse from './chat-response'
+"use client";
 
-type Props = {}
+import React, {useRef, useEffect } from 'react';
+import ChatResponse from './chat-response';
+import ChatRequest from './chat-request';
 
-const ChatSection = (props: Props) => {
-    return (
-        <div className='flex flex-col max-w-3xl mx-auto w-full p-4 space-y-4'>
-            <ChatRequest text='What are the Hyperledger Fabric WSL2 install instructions?' />
-            <ChatResponse text='Both the Fabric documentation and Fabric samples rely heavily on a bash environment. The recommended path is to use WSL2 (Windows Subsystem for Linux version 2) to provide a native Linux environment and then you can follow the Linux prerequisites section (excluding the Linux Docker prerequisite as you already have Docker Desktop) and install them into your WSL2 linux distribution. WSL2 may not be installed by default; you can check and install WSL2 by going into “Programs and Features”, clicking on “Turn Windows features on or off” and ensuring that both “Windows Subsystem For Linux” and “Virtual Machine Platform” are selected. Next you will need to install a Linux distribution such as Ubuntu-20.04 and make sure it’s set to using version 2 of WSL. Refer to Install WSL for more information. Finally, you need to ensure Docker Desktop has integration enabled for your distribution so it can interact with Docker elements, such as a bash command window. To do this, open the Docker Desktop gui and go into settings, select Resources and them WSL Integration and ensure the checkbox for enable integration is checked. You should then see your WSL2 linux distribution listed (if you don’t then it is probably because it is still a WSL1 distribution and needs to be converted to WSL2) and you can then toggle the switch to enable integration for that distro. Refer to Docker Desktop WSL2 backend for more information' />
-        </div>
-    )
+interface Message {
+    request: string;
+    response: string;
 }
 
-export default ChatSection
+interface ChatSectionProps {
+    messages: Message[];
+}
+
+const ChatSection: React.FC<ChatSectionProps> = ({ messages }) => {
+    const chatContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
+
+    return (
+        <div className='relative flex flex-col items-center h-screen overflow-hidden'>
+            <div className='flex flex-col max-w-3xl w-full p-4 space-y-4 flex-grow overflow-y-auto' ref={chatContainerRef} style={{ paddingBottom: '5rem' }}>
+                {messages.map((msg, index) => (
+                    <div key={index}>
+                        <ChatRequest request={msg.request} /> 
+                        <ChatResponse response={msg.response} /> 
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ChatSection;
