2574e990b3d5edd6cb7ff2360c760392c77dd43f
Sauradip Ghosh <sauradip96ghosh@gmail.com>
2024-10-02 21:37:54 +0530

    Add Sidebar history Section (#50)
    * Add sidebar history and submit button ui change
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * Format Fix
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * Span and Overflow added
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * Rename files
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * convert checkboxes to button
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * button's class a state variable added
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * Remove truncateTitle
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    * Format Fix
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>
    
    ---------
    
    Signed-off-by: Sauradip Ghosh <sauradip96ghosh@gmail.com>

---
+++ src/frontend/components/history-data.ts
@@ -0,0 +1,22 @@
+export const historyData = [
+	{
+		id: 1,
+		title: 'What is hyperledger fabric'
+	},
+	{
+		id: 2,
+		title: 'How to install hyperledger fabric'
+	},
+	{
+		id: 3,
+		title: 'How to deploy hyperledger fabric'
+	},
+	{
+		id: 4,
+		title: 'How to run hyperledger fabric'
+	},
+	{
+		id: 5,
+		title: 'How to ensure data privacy'
+	}
+];
