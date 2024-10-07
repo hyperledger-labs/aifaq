b035d79b880e07d64cc744bb4055d756bf35c93a
SarveshAtawane <sarveshatawane03@gmail.com>
2024-10-08 03:01:05 +0530

    cleaned reposne
    Signed-off-by: SarveshAtawane <sarveshatawane03@gmail.com>

--- src/frontend/lib/apis/fetcher.ts
+++ src/frontend/lib/apis/fetcher.ts
@@ -4,16 +4,13 @@ import { Message } from '@/lib/types';
 export async function handleSend(
 	message: Message
 ) {
-	return fetch(
-		SERVER_BASE_URL + '/query',
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(message)
-		}
-	)
+	return fetch(SERVER_BASE_URL + '/query', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(message)
+	})
 		.then((response) => {
 			return response.json();
 		})
