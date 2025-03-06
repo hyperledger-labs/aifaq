13b08126cf31d4937255748fa0fc9ce11806ee60
Gianluca Capuzzi <gianluca.posta78@gmail.com>
2025-03-06 17:57:33 +0100

    Removed client folder on mvt-streamlit branch
    Signed-off-by: Gianluca Capuzzi <gianluca.posta78@gmail.com>

--- src/client/lib/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Message {
-    role: "user" | "assistant";
-    content: string;
-  }
-  
-  export type CodeBlockTheme = 'dark' | 'light' | 'dracula' | 'github' | 'monokai' | 'nord';
